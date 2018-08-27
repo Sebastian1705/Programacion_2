da6c2af7b0f3b63015685b8857878510300f4a85
Seba <sebaaguirre2012@gmail.com>
2018-08-26 22:32:31 -0300

    Ejercio 8 de guia

---
+++ Clase_01/Ejercicio_07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ejercicio_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_07";
+            DateTime localDate = DateTime.Now;
+            DateTime fechaNacimiento;
+            TimeSpan diasVividos;
+            string fecha;
+            int cantidadDias;
+            Console.Write("Ingrese fecha de nacimiento: ");
+            fecha = Console.ReadLine();
+            DateTime.TryParse(fecha, out fechaNacimiento);
+            diasVividos = localDate - fechaNacimiento;
+            cantidadDias = diasVividos.Days;
+
+            Console.WriteLine("La fecha de nacimiento es: {0}", fechaNacimiento);
+            Console.WriteLine("La fecha actual es: {0}", localDate);
+            Console.WriteLine("La cantidad de dias vividos es: {0}", cantidadDias);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase_01/Ejercicio_08/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ejercicio_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_08";
+            string nombre, auxHorasTrabajadas, auxValorHora, auxAntiguedad, cantidadEmpleados;
+            int cantidad, sumador = 0, importeBruto = 0, importeNeto=0, descuentos=0, valorHora, antiguedad = 0, horasTrabajadas;
+
+            Console.Write("Ingrese la cantidad de empleados: ");
+            cantidadEmpleados = Console.ReadLine();
+
+            if(int.TryParse(cantidadEmpleados, out cantidad))
+            {
+                do
+                {
+                    Console.Write("Ingrese el nombre: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Ingrese el valor de la hora: ");
+                    auxValorHora = Console.ReadLine();
+                    Console.Write("Ingrese la antiguedad: ");
+                    auxAntiguedad = Console.ReadLine();
+                    Console.Write("Ingrese la cantidad de horas trabajadas: ");
+                    auxHorasTrabajadas = Console.ReadLine();
+                    sumador++;
+
+                    if(int.TryParse(auxValorHora, out valorHora) && int.TryParse(auxAntiguedad, out antiguedad) && int.TryParse(auxHorasTrabajadas, out horasTrabajadas))
+                    {
+                        importeBruto = (valorHora * horasTrabajadas) + (antiguedad * 150);
+                        descuentos = importeBruto * 13 / 100;
+                        importeNeto = importeBruto - descuentos;
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Nombre: {0} - Atiguedad: {1} - Valor hora: ${2} - Importe bruto: ${3} - Descuentos: ${4} - Importe neto: ${5}", nombre,
+                                                                                                                                                   antiguedad,
+                                                                                                                                                   valorHora,
+                                                                                                                                                   importeBruto,
+                                                                                                                                                   descuentos,
+                                                                                                                                                   importeNeto);
+                    Console.WriteLine();
+                } while (sumador < cantidad);
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
