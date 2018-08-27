0632a5bb53a3ca5d9aedcf071c84d8f04dd03927
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-08-27 12:28:57 -0300

    Guia ejercicio 10

---
+++ Clase_01/Ejercicio_09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_09";
+            int numeroIngresado;
+            string auxNumero, imprimir = "*";
+            Console.Write("Ingrese la cantidad de pisos de la piramide: ");
+            auxNumero = Console.ReadLine();
+
+            if(int.TryParse(auxNumero, out numeroIngresado))
+            {
+                for(int i = 0; i<numeroIngresado; i++)
+                {
+                    Console.WriteLine(imprimir);
+                    Console.WriteLine();
+                    imprimir += "**";
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase_01/Ejercicio_10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_10";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
