cc30354be8f37ba767a09884187aa610b4f6c2da
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-09-10 20:44:34 -0300

    Windows form

---
+++ Clase_06/Clase_06/frmFormulario3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_06
+{
+    public partial class frmFormulario3 : Form
+    {
+        public frmFormulario3()
+        {
+            InitializeComponent();
+        }
+    }
+}
