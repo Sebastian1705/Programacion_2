bc0e4a44d576dc9ace2a1e3622851157f38d7555
Seba <sebaaguirre2012@gmail.com>
2018-09-17 21:46:25 -0300

    clase 08

---
+++ Clase_08/Clase_08/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Clase_08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(12, 12);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(113, 45);
+            this.btAceptar.TabIndex = 0;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAceptar;
+    }
+}
+
---
+++ Clase__08/Clase__08/Factura.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase__08
+{
+    public class Factura
+    {
+        private int numero;
+        private DateTime fecha;
+        private int moneda;
+        public List<Producto> productos;
+
+        public Factura()
+        {
+
+        }
+
+        public Factura(int numero, int moneda, DateTime fecha)
+        {
+            this.numero = numero;
+            this.moneda = moneda;
+            this.fecha = fecha;
+            this.productos = new List<Producto>();
+        }
+
+        public double GetTotal()
+        {
+            double total = 0;
+            foreach(Producto item in this.productos)
+            {
+               
+            }
+            return total;
+        }
+    }
+}
