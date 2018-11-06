92a13c255772355cb3d1315b720ccdacf682dc84
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-11-05 21:52:13 -0300

    ...

---
+++ Hilos/Hilos/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Hilos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(Sumar));
+
+            t1.Start();
+        }
+
+        int[] numero = { 0, 0, 0, 0 };
+       
+        private void Sumar(object o)
+        {
+            int posicion = (int)o;
+            while(numero[posicion] < 10000)
+            {
+
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
