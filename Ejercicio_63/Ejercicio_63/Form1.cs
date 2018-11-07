f7e0317b439f31df5749499a5988603a5cf1ba64
alumno <alumno>
2018-11-06 22:06:32 -0300

    Ejercicio 64

---
+++ Ejercicio_63/Ejercicio_63/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+
+namespace Ejercicio_63
+{
+    public partial class Form1 : Form
+    {
+        Thread hilo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitializeTimer();
+        }
+
+
+        private void lblHora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hilo = new Thread(MostrarHora);
+            hilo.Start();
+        }
+
+        private void MostrarHora()
+        {
+            while (true)
+            {
+                if (this.lblHora.InvokeRequired)
+                {
+                    this.lblHora.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        this.lblHora.Text = DateTime.Now.ToString();
+                    });
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("G");
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+        }
+    }
+}
---
+++ Ejercicio_64/Entidades/Negocio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Negocio
+    {
+        private Caja caja1;
+        private Caja caja2;
+        private List<string> clientes;
+
+        public Caja Caja1
+        {
+            get
+            {
+                return this.caja1;
+            }
+        }
+
+        public Caja Caja2
+        {
+            get
+            {
+                return this.caja2;
+            }
+        }
+
+        public List<string> Clientes
+        {
+            get
+            {
+                return this.clientes;
+            }
+        }
+
+        public Negocio(Caja caja1, Caja caja2)
+        {
+            this.caja1 = caja1;
+            this.caja2 = caja2;
+            this.clientes = new List<string>();
+        }
+
+        public void AsignarCajas()
+        {
+            Console.WriteLine("Asignando cajas...");
+            foreach(string item in Clientes)
+            {
+                if (Caja1.FilaClientes.Count < Caja2.FilaClientes.Count)
+                    Caja1.FilaClientes.Add(item);
+                else
+                    Caja2.FilaClientes.Add(item);
+                Thread.Sleep(1000);
+            }       
+        }
+
+    }
+}
