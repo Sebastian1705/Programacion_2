0fd2eab48073def9cda4d13dbe37d6e41b8d20ea
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-11-27 15:01:12 -0300

    ...

---
+++ Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/Producto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Producto
+    {
+        #region Fields
+
+        private string descripcion;
+
+        #endregion
+
+        #region Propieties
+
+        public string Descripcion
+        {
+            get
+            {
+                return this.descripcion;
+            }
+            set
+            {
+                this.descripcion = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Producto()
+        {
+
+        }
+
+        public Producto(string descripcion)
+            : this()
+        {
+            this.Descripcion = descripcion;
+        }
+
+        /// <summary>
+        /// Mostrar en producto tendrá implementación y podrá ser sobrescrito en las clases hijas.
+        /// Deberá retornar un string con el formato: DESCRIPCIÓN: {descripcio}.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return String.Format($"DESCRIPCIÓN: {this.Descripcion}.");
+        }
+
+        /// <summary>
+        /// El método Elaborar deberá guardar en la base de datos el producto y 
+        /// disparar el evento InformarProductoTerminado informando el producto elaborado.
+        /// </summary>
+        public void Elaborar()
+        {
+            ProductoDAO.GuardarProducto(this);
+            this.InformarProductoTerminado.Invoke(this, new EventArgs());
+        }
+
+        #endregion
+
+        #region Delegate
+
+        public delegate void ProductoTerminado(Object sender, EventArgs e);
+
+        #endregion
+
+        #region Events
+
+        public event ProductoTerminado InformarProductoTerminado;
+
+        #endregion
+    }
+
+    public enum Material
+    {
+        Plastico, Aluminio, Caucho
+    }
+
+}
---
+++ Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/ProductoDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Entidades
+{
+    /*
+    i. Crear una nueva base de datos con el nombre final-20180712 y crear la tabla Productos
+    ejecutando el archivo .sql provisto.
+    ii. Tener en cuenta:
+    Al insertar un “ProductoA” los campos correspondientes a un “ProductoB” quedarán en null
+    y viceversa.
+    En el campo “tipo” insertar “A” para instancias de ProductoA y “B” para instancias de
+    “ProductoB”
+     * */
+    public static class ProductoDAO
+    {
+        #region Fields
+
+        private static string cadenaDeConexion;
+        private static SqlCommand comando;
+        private static SqlConnection conexion;
+
+        #endregion
+
+        #region Methods
+
+        static ProductoDAO()
+        {
+            comando = new SqlCommand();
+            conexion = new SqlConnection(@cadenaDeConexion);
+            comando.CommandType = CommandType.Text;
+            comando.Connection = conexion;
+        }
+
+        public static List<Producto> ObtenerProdustos()
+        {
+            List<Producto> l = new List<Producto>();
+
+            return l;
+        }
+
+        public static bool GuardarProducto(Producto p)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
