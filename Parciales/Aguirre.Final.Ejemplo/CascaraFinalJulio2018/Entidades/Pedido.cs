8d765ad6d620bb6bc2eed5f2095f1e93f0ebf466
Sebastian1705 <sebaaguirre2012@gmail.com>
2018-11-28 14:59:28 -0300

    final

--- Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/Pedido.cs
+++ Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/Pedido.cs
@@ -9,6 +9,81 @@ namespace Entidades
 {
     public class Pedido
     {
-       
+        #region Fields
+
+        private List<Producto> productos;
+
+        #endregion
+
+        #region Propieties
+
+        public List<Producto> Productos
+        {
+            get
+            {
+                return this.productos;
+            }
+            set
+            {
+                this.productos = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Pedido()
+        {
+            this.Productos = new List<Producto>();
+        }
+
+        /// <summary>
+        /// FabricarPedido recorrerá la lista de productos, por cada uno
+        /// simular un retardo de 1 segundo y luego elaborar el producto
+        /// </summary>
+        public void FabricarPedido()
+        {
+            foreach(Producto item in this.Productos)
+            {
+                System.Threading.Thread.Sleep(1000);
+                item.Elaborar();
+            }
+        }
+
+        /// <summary>
+        /// listar y retornar
+        /// un string con los datos de todos los productos incluidos en el pedido.
+        /// </summary>
+        /// <param name="unPedido"></param>
+        public static implicit operator string(Pedido unPedido)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Producto item in unPedido.Productos)
+            {
+                sb.AppendLine($"{item.Mostrar()}");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// El operador + agregara un producto al pedido teniendo en cuenta que cada pedido puede
+        /// contener un máximo de 5 productos. (Sólo primer parcial y final: Adicionalmente validar las
+        /// dimensiones del producto antes de agregarlo al pedido).
+        /// </summary>
+        /// <param name="unPedido"></param>
+        /// <param name="unProducto"></param>
+        /// <returns></returns>
+        public static Pedido operator +(Pedido unPedido, Producto unProducto)
+        {
+            if(!(unProducto is null) && unPedido.Productos.Count < 6)
+            {
+                if(unProducto.ValidarDimensiones())
+                    unPedido.Productos.Add(unProducto);
+            }
+            return unPedido;
+        }
+
+        #endregion
     }
 }
--- Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/ProductoB.cs
+++ Parciales/Aguirre.Final.Ejemplo/CascaraFinalJulio2018/Entidades/ProductoB.cs
@@ -6,8 +6,101 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class ProductoB
+    public class ProductoB : Producto
     {
+        #region Fields
 
+        private short alto;
+        private short ancho;
+        private short largo;
+
+        #endregion
+
+        #region Propieties
+
+        public short Alto
+        {
+            get
+            {
+                return this.alto;
+            }
+        }
+
+        public short Ancho
+        {
+            get
+            {
+                return this.ancho;
+            }
+        }
+
+        public short Largo
+        {
+            get
+            {
+                return this.largo;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public ProductoB()
+            : base()
+        {
+
+        }
+
+        public ProductoB(string descripcion, short largo, short alto, short ancho)
+            : base(descripcion)
+        {
+            this.largo = largo;
+            this.ancho = ancho;
+            this.alto = alto;
+        }
+
+        /// <summary>
+        /// Mostrar retornará una cadena con todos los datos del producto incluyendo el volumen.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            return String.Format($"{base.Mostrar()} Tipo: B, ANCHO: {this.Ancho}, ALTO: {this.Alto}, LARGO: {this.Largo}, VOLUMEN: {this.CalcularVolumen()}.");
+        }
+
+        /// <summary>
+        /// ValidarDimensiones retornará true si la suma del largo, ancho y
+        /// alto no supera los 100 cm.
+        /// </summary>
+        /// <returns></returns>
+        public override bool ValidarDimensiones()
+        {
+            bool retorno = false;
+            if ((this.Ancho + this.Largo + this.Alto) < 101)
+                retorno = true;
+            return retorno;
+        }
+
+        /// <summary>
+        /// CalcularVolumen retornará el volumen obtenido de la multiplicación entre el alto, largo y
+        /// ancho.
+        /// </summary>
+        private int CalcularVolumen()
+        {
+            return (this.Alto * this.Ancho * this.Largo);
+        }
+
+        /// <summary>
+        /// Sobrecargar el operador explicit donde se retornará el volumen del
+        /// producto.
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator int(ProductoB p)
+        {
+            return p.CalcularVolumen();
+        }
+
+        #endregion
     }
 }
